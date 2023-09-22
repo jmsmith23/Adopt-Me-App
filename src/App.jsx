70b7805c5f7bf937dfa68a1a231db01d7624bb6d
Jeff Smith <127367482+jmsmith23@users.noreply.github.com>
2023-09-22 16:54:31 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Frank",
+      animal: "Cat",
+      breed: "Tabby",
+    }),
+    React.createElement(Pet, {
+      name: "Ruby",
+      animal: "Dog",
+      breed: "German Shepard",
+    }),
+    React.createElement(Pet, {
+      name: "Rizzo",
+      animal: "Rat",
+      breed: "Alley-Rat",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
