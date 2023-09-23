7e4f4dae118e949bb91a60aa7c03a929b183fc5e
Jeff Smith <127367482+jmsmith23@users.noreply.github.com>
2023-09-22 23:58:22 -0500

    create custom hook to handle animal breeds based on selected animal

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,9 +1,11 @@
 const Pet = (props) => {
-  <div>
-    <h1>{props.name}</h1>
-    <h2>{props.animal}</h2>
-    <h2>{props.breed}</h2>
-  </div>;
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  );
 };
 
 export default Pet;
